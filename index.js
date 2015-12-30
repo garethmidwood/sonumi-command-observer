5558634bee69b5f1cd2ae774dea472ab9ba7231a
Gareth Midwood <gmidwood@inviqa.com>
2015-12-30 16:23:21 +0000

    Add command observer code

---
+++ index.js
@@ -0,0 +1,168 @@
+var extend = require('extend');
+var config = require('config');
+var sonumiLogger = require('sonumi-logger');
+
+const COMMAND_PUBLICATION_NAME = "pub_commands";
+const RESPONSE_EXECUTING = 'EXECUTING';
+const RESPONSE_COMPLETE = 'COMPLETE';
+const RESPONSE_FAIL = 'FAIL';
+
+var client,
+    logger;
+
+function Observer(connector)
+{
+    var logDirectory = config.logging.logDir;
+    logger = sonumiLogger.init(logDirectory);
+    logger.addLogFile('info', logDirectory + '/command-observer-info.log', 'info');
+    logger.addLogFile('errors', logDirectory + '/command-observer-error.log', 'error');
+
+    client = connector;
+
+    // subscribe to the publication
+    client.subscribe(COMMAND_PUBLICATION_NAME);
+
+    // observe the publication
+    var observer = client.observe(COMMAND_PUBLICATION_NAME);
+    extend(observer, this);
+
+    // return the extended observer
+    return observer;
+}
+
+Observer.prototype = {
+    handlers: {},
+    register_handler: function(label, handler) {
+        this.handlers[label] = handler;
+    },
+    // run when a command is added
+    added: function(_id) {
+        var command = retrieveCommandFromCollection(_id);
+
+        logger.log('[ADDED] ' + command + ' [' + _id + ']');
+
+        var commandParts = command.text.split('.');
+
+        // make sure we have the correct number of parts for the command
+        if (commandParts.length != 3) {
+            logger.error('[ADDED] Incorrect number of parameters in command: ' + command + ' [' + _id + ']');
+            this.status_fail(_id);
+            return;
+        }
+
+        var device  = commandParts[0];
+        var handler = commandParts[1];
+        var action  = commandParts[2];
+
+        // acknowledge receipt of command
+        this.status_ack(_id);
+
+        execute.call(this, _id, device, handler, action);
+    },
+    changed: function changed(id, oldFields, clearedFields, newFields) {
+        logger.log('[CHANGED] old field values: ' + JSON.stringify(oldFields));
+        logger.log('[CHANGED] cleared fields: ' + JSON.stringify(clearedFields));
+        logger.log('[CHANGED] new fields: ' + JSON.stringify(newFields));
+    },
+    removed: function removed(id, oldValue) {
+        logger.log('[REMOVED] ' + JSON.stringify(oldValue));
+    },
+
+
+    status_ack: function(id) {
+        client.call(
+            'acknowledgeCommand',
+            [id],
+            function (err, result) {
+                if (err) {
+                    logger.error(
+                        'Error acknowledging command ID ' + id + ' message: ' + JSON.stringify(err)
+                    );
+                }
+            }
+        );
+    },
+    status_complete: function(id) {
+        client.call(
+            'successCommand',
+            [id],
+            function (err, result) {
+                if (err) {
+                    logger.error(
+                        'Error completing command ID ' + id + ' message: ' + JSON.stringify(err)
+                    );
+                }
+            }
+        );
+    },
+    status_executing: function(id) {
+        client.call(
+            'alreadyRunningCommand',
+            [id],
+            function (err, result) {
+                if (err) {
+                    logger.error(
+                        'Error completing already running command ID ' + id + ' message: ' + JSON.stringify(err)
+                    );
+                }
+            }
+        );
+    },
+    status_fail: function(id) {
+        client.call(
+            'failedCommand',
+            [id],
+            function (err, result) {
+                if (err) {
+                    logger.error(
+                        'Error logging failure to process command ID ' + id + ' message: ' + JSON.stringify(err)
+                    );
+                }
+            }
+        );
+    }
+};
+
+
+function execute(_id, device, handler, action) {
+    switch (device) {
+        case 'sonumi':
+            // TODO: store list of connected devices in Mongo
+            // connected devices should automatically update on the server
+            // with this mongo collection.
+            // this 'sonumi' case should be used for setting up schedules
+            logger.log('requested connected devices list');
+            this.status_complete(_id);
+            break;
+        default:
+            var self = this;
+
+            // run command if handler/action exist
+            if (this.handlers[handler] && this.handlers[handler][action]) {
+                this.handlers[handler][action](function (responseCode) {
+                    switch (responseCode) {
+                        case RESPONSE_EXECUTING:
+                            self.status_executing(_id);
+                            break;
+                        case RESPONSE_COMPLETE:
+                            self.status_complete(_id);
+                            break;
+                        case RESPONSE_FAIL:
+                        default:
+                            self.status_fail(_id);
+                            break;
+                    }
+                });
+            } else {
+                this.status_fail(_id);
+            }
+            break;
+    }
+}
+
+function retrieveCommandFromCollection(id) {
+    return client.collections.commands[id];
+}
+
+
+module.exports = Observer;
---
+++ test/index.js
@@ -0,0 +1,216 @@
+var expect = require('chai').expect,
+    assert = require('chai').assert,
+    sinon  = require('sinon'),
+    rewire = require('rewire');
+
+
+describe("Setup", function() {
+    var observer,
+        loggerMock,
+        sonumiLoggerMock,
+        clientMock,
+        commandObserver = rewire("../index");
+
+    beforeEach(function() {
+        loggerMock = sinon.stub();
+        loggerMock.log = sinon.stub();
+        loggerMock.addLogFile = sinon.stub();
+        sonumiLoggerMock = sinon.stub();
+        sonumiLoggerMock.init = sinon.stub().returns(loggerMock);
+
+        configMock = {
+            "logging": {
+                "logDir": "/tmp/"
+            }
+        };
+
+        commandObserver.__set__({
+            config: configMock,
+            logger: loggerMock,
+            sonumiLogger: sonumiLoggerMock
+        });
+
+        clientMock = sinon.stub();
+        clientMock.subscribe = sinon.spy();
+        clientMock.observe = sinon.stub().returns({});
+        clientMock.collections = {'commands': []};
+
+        observer = new commandObserver(clientMock);
+    });
+
+    it('should subscribe to the collection', function() {
+        assert(clientMock.subscribe.calledWith('pub_commands'));
+    });
+
+    it('should observe the collection', function() {
+        assert(clientMock.observe.calledWith('pub_commands'));
+    });
+
+    it('should register new handlers', function() {
+        var label = 'test_handler';
+
+        expect(observer.handlers[label]).to.be.undefined;
+
+        observer.register_handler(label, {});
+
+        expect(observer.handlers[label]).to.not.be.undefined;
+    });
+});
+
+
+
+describe("Observe Commands", function() {
+    var observer,
+        loggerMock,
+        sonumiLoggerMock,
+        clientMock,
+        commandObserver = rewire("../index");
+
+    beforeEach(function() {
+        loggerMock = sinon.stub();
+        loggerMock.log = sinon.stub();
+        loggerMock.error = sinon.stub();
+        loggerMock.addLogFile = sinon.stub();
+        sonumiLoggerMock = sinon.stub();
+        sonumiLoggerMock.init = sinon.stub().returns(loggerMock);
+
+        configMock = {
+            "logging": {
+                "logDir": "/tmp/"
+            }
+        };
+
+        commandObserver.__set__({
+            config: configMock,
+            logger: loggerMock,
+            sonumiLogger: sonumiLoggerMock,
+            execute: sinon.spy()
+        });
+
+        clientMock = sinon.stub();
+        clientMock.subscribe = sinon.spy();
+        clientMock.observe = sinon.stub().returns({});
+        clientMock.collections = {
+            'commands': [
+                {'text': 'my.test'},
+                {'text': 'my.test.action'}
+            ]
+        };
+
+        observer = new commandObserver(clientMock);
+    });
+
+    it('should reject badly formatted commands', function() {
+        var commandId = 0;
+
+        observer.status_fail = sinon.spy();
+
+        observer.added(commandId);
+
+        assert(observer.status_fail.calledWith(commandId));
+    });
+
+    it('should acknowledge and execute new commands', function() {
+        var commandId = 1;
+
+        observer.status_ack = sinon.spy();
+
+        observer.added(commandId);
+
+        assert(observer.status_ack.calledWith(commandId));
+        assert(commandObserver.__get__('execute').calledWith(commandId, 'my', 'test', 'action'));
+    });
+});
+
+describe("Handlers", function() {
+    var observer,
+        loggerMock,
+        sonumiLoggerMock,
+        clientMock,
+        commandObserver = rewire("../index");
+
+    beforeEach(function() {
+        loggerMock = sinon.stub();
+        loggerMock.log = sinon.stub();
+        loggerMock.error = sinon.stub();
+        loggerMock.addLogFile = sinon.stub();
+        sonumiLoggerMock = sinon.stub();
+        sonumiLoggerMock.init = sinon.stub().returns(loggerMock);
+
+        configMock = {
+            "logging": {
+                "logDir": "/tmp/"
+            }
+        };
+
+        commandObserver.__set__({
+            config: configMock,
+            logger: loggerMock,
+            sonumiLogger: sonumiLoggerMock
+        });
+
+        clientMock = sinon.stub();
+        clientMock.subscribe = sinon.spy();
+        clientMock.observe = sinon.stub().returns({});
+        clientMock.collections = {
+            'commands': [
+                {'text': 'my.test'},
+                {'text': 'my.test.action'}
+            ]
+        };
+
+        observer = new commandObserver(clientMock);
+        observer.status_ack = sinon.spy();
+    });
+
+    it('should acknowledge and fail when no handler is found for a command', function() {
+        var commandId = 1;
+
+        observer.status_fail = sinon.spy();
+
+        observer.added(commandId);
+
+        assert(observer.status_ack.calledWith(commandId));
+        assert(observer.status_fail.calledWith(commandId));
+    });
+
+    it('should acknowledge and fail when the command handler returns a failure status', function() {
+        var commandId = 1;
+
+        observer.status_fail = sinon.spy();
+
+        observer.register_handler('test', {'action': sinon.stub().callsArgWith(0, 'FAIL') });
+
+        observer.added(commandId);
+
+        assert(observer.status_ack.calledWith(commandId));
+        assert(observer.status_fail.calledWith(commandId));
+    });
+
+    it('should acknowledge and continue when the command handler returns an executing status', function() {
+        var commandId = 1;
+
+        observer.status_executing = sinon.spy();
+
+        observer.register_handler('test', {'action': sinon.stub().callsArgWith(0, 'EXECUTING') });
+
+        observer.added(commandId);
+
+        assert(observer.status_ack.calledWith(commandId));
+        assert(observer.status_executing.calledWith(commandId));
+    });
+
+    it('should acknowledge and complete when the command handler returns a complete status', function() {
+        var commandId = 1;
+
+        observer.status_complete = sinon.spy();
+
+        observer.register_handler('test', {'action': sinon.stub().callsArgWith(0, 'COMPLETE') });
+
+        observer.added(commandId);
+
+        assert(observer.status_ack.calledWith(commandId));
+        assert(observer.status_complete.calledWith(commandId));
+    });
+});
+
